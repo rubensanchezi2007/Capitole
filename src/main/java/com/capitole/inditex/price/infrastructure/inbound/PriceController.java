c9ba3affe0ff5b6141fc199db499a0bfd5ac8b17
Ruben Sanchez <ruben.sanchezi2007@gmail.com>
2025-04-28 08:16:30 +0200

    ApiFirst & Hexagonal folder structure

---
+++ src/main/java/com/capitole/inditex/price/infrastructure/inbound/PriceController.java
@@ -0,0 +1,9 @@
+package com.capitole.inditex.price.infrastructure.inbound;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PriceController {
+
+
+}
