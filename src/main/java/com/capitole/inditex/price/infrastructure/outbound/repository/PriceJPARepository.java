aadceb2f33560c3742e1f5d2de4c074ba99e7838
Ruben Sanchez <ruben.sanchezi2007@gmail.com>
2025-05-08 10:44:16 +0200

    another unnecesary public removed

--- src/main/java/com/capitole/inditex/price/infrastructure/outbound/repository/PriceJPARepository.java
+++ src/main/java/com/capitole/inditex/price/infrastructure/outbound/repository/PriceJPARepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface PriceJPARepository extends JpaRepository<PriceEntity, Long> {
     @Query("select p from PriceEntity p where p.product_id = ?1 AND p.brand.id= ?2 AND (p.start_date<?3 AND p.end_date>?3)")
-    public List<PriceEntity> findPriceByDate(Long productId, Integer brandId, LocalDateTime date);
+     List<PriceEntity> findPriceByDate(Long productId, Integer brandId, LocalDateTime date);
 }
